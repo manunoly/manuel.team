434015b4043f24e3c76ffd68c5a52ce3159b370e
Manuel Almaguer <manunoly@gmail.com>
2025-11-21 10:00:59 -0500

    update portfolio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,13 +13,21 @@ interface Project {
 
 interface ProjectsProps {
     projects: Project[];
+    lang: string;
 }
 
-export default function Projects({ projects }: ProjectsProps) {
+export default function Projects({ projects, lang = 'en' }: ProjectsProps) {
     return (
         <SectionContainer id="projects">
             <h2 className="flex items-center text-3xl font-bold text-app-text mb-12">
-                <span className="text-app-accent mr-2">03.</span> Some Things I've Built
+                <span className="text-app-accent mr-2">03.</span>
+                {
+                    lang === 'en' ? (
+                        <span>Some Projects I've Built or Collaborated On</span>
+                    ) : (
+                        <span>Algunos Proyectos que desarrollado o Colaborado</span>
+                    )
+                }
                 <span className="ml-4 h-px bg-app-surface flex-grow max-w-xs"></span>
             </h2>
 
@@ -35,7 +43,7 @@ export default function Projects({ projects }: ProjectsProps) {
                             <img
                                 src={project.image}
                                 alt={project.title}
-                                className="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500"
+                                className="w-full h-full object-contain transform group-hover:scale-110 transition-transform duration-500"
                             />
                         </div>
 
@@ -45,10 +53,10 @@ export default function Projects({ projects }: ProjectsProps) {
                                     {project.title}
                                 </h3>
                                 <div className="flex space-x-4 text-app-text-muted">
-                                    <a href={project.github} className="hover:text-app-accent transition-colors">
+                                    <a href={project.github} target='_blank' className="hover:text-app-accent transition-colors">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
                                     </a>
-                                    <a href={project.link} className="hover:text-app-accent transition-colors">
+                                    <a href={project.link} target='_blank' className="hover:text-app-accent transition-colors">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
                                     </a>
                                 </div>
